17f0ce4ad11d96194324fa8c0448aa21f9cda6e4
Glenn Tesoriero <glenn.tesoriero@outlook.com>
2023-10-06 13:31:20 +1100

    update

--- 7-azure-devops-workspaces/workspacetest.sh
+++ 7-azure-devops-workspaces/workspacetest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 echo "*********** Create or select workspace"
-if [ $(terraform workspace list | grep -c "$1") -eq 0 ] ; then
-  echo "Create new workspace $1"
-  terraform workspace new "$1" -no-color
-else
-  echo "Switch to workspace $1"
-  terraform workspace select "$1" -no-color
-fi
+WORKSPACECOUNT=$(terraform workspace list | grep -c "$1")
+echo "WORKSPACECOUNT is $WORKSPACECOUNT"
+echo "The workspace name is $1"
+#if [ WORKSPACECOUNT -eq 0 ] ; then
+echo "Create new workspace $1"
+terraform workspace new "$1"
+#else
+#  echo "Switch to workspace $1"
+#  terraform workspace select "$1" -no-color
+#fi
